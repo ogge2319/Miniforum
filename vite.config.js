4299de298f478e70573a5203584fd13834b33d6a
Hodman Liban <Hodman_libanh@hotmail.com>
2025-11-11 12:26:44 +0100

    config change

--- vite.config.js
+++ vite.config.js
@@ -3,16 +3,19 @@ import react from '@vitejs/plugin-react'
 import fs from 'fs'
 import path from 'path'
 
+const isDev = process.env.NODE_ENV === 'development' || process.env.NETLIFY !== 'true';
+
 export default defineConfig({
   plugins: [react()],
 
   server: {
-    https: {
-      key: fs.readFileSync(path.resolve(__dirname, 'certs/localhost.key')),
-      cert: fs.readFileSync(path.resolve(__dirname, 'certs/localhost.crt')),
-    },
+    ...(isDev && {
+      https: {
+        key: fs.readFileSync(path.resolve(__dirname, 'certs/localhost.key')),
+        cert: fs.readFileSync(path.resolve(__dirname, 'certs/localhost.crt')),
+      },
+    }),
     port: 5173,
-
     proxy: {
       // AUTH BACKEND - Mer specifik regel först
       '/api/auth': {
